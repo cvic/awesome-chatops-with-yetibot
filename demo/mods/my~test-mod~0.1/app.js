97679b840cb5ed2dbca820431dc85ded999d4624
Vic <cvctor@gmail.com>
2017-07-28 08:09:53 +0300

    Additional cleanup

--- demo/mods/my~test-mod~0.1/app.js
+++
@@ -1,9 +0,0 @@
-var container = require('vertx/container');
-
-var webServerConf = {
-    port: 8888,
-    host: 'localhost',
-    "web_root": "."
-};
-
-container.deployModule('io.vertx~mod-web-server~2.0.0-final', webServerConf);
--- demo/server.js
+++
@@ -1,5 +0,0 @@
-var vertx = require('vertx');
-
-vertx.createHttpServer().requestHandler(function(req) {
-    req.response.end("Hi there, STLJS!");
-}).listen(8090, 'localhost');
